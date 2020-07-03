c388afdc7f7ce832e43443c436548858f4ec512b
kashish batra <63490177+kashba1@users.noreply.github.com>
2020-07-03 19:51:47 +0530

    added initial dice game's files  .

---
+++ index.js
@@ -0,0 +1,26 @@
+var p1 = "Player1";
+var p2 = "Player2";
+
+
+
+function start(){
+
+var randomNumber1=Math.floor(Math.random()*6+1);
+var randImg="images/dice" + randomNumber1 + ".png"
+document.querySelector(".img1").setAttribute("src",randImg);
+
+var randomNumber2=Math.floor(Math.random()*6+1);
+var randImg2="images/dice" + randomNumber2 + ".png"
+document.querySelector(".img2").setAttribute("src",randImg2);
+
+// code to decide who wins
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").textContent="🚩 player 1 Wins";
+}
+else if(randomNumber1<randomNumber2){
+  document.querySelector("h1").textContent= "player 2 Wins 🚩";
+}
+else {
+  document.querySelector("h1").textContent="Draw!";
+}
+}
